b35a590fd15fa101d6ef6f12d28e8414abbc710d
cachi1997 <cachigaliano1997@gmail.com>
2025-04-29 19:37:36 -0300

    Refactor index.html and App.tsx structure, remove unused react.svg, and implement BudgetForm component

---
+++ src/components/BudgetForm.tsx
@@ -0,0 +1,44 @@
+import { useMemo, useState } from "react";
+
+const BudgetForm = () => {
+  const [budget, setBudget] = useState(0);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBudget(e.target.valueAsNumber);
+  };
+
+  const isValid = useMemo(() => {
+    return isNaN(budget) || budget <= 0;
+  }, [budget]);
+
+  return (
+    <form className="space-y-5">
+      <div className="flex flex-col space-y-5">
+        <label
+          htmlFor="budget"
+          className="text-4xl text-blue-600 font-bold text-center"
+        >
+          Definir presupuesto
+        </label>
+        <input
+          id="budget"
+          type="number"
+          className="w-full bg-white border border-gray-200 p-2"
+          placeholder="Añade tu presupuesto"
+          name="budget"
+          value={budget}
+          onChange={(e) => handleChange(e)}
+        />
+      </div>
+      <input
+        type="submit"
+        value="Añadir"
+        className="bg-blue-600 hover:bg-blue-700 text-white cursor-pointer font-black w-full p-2 uppercase rounded-lg transition-colors duration-300 disabled:opacity-40 disabled:cursor-not-allowed"
+        name="submit"
+        disabled={isValid}
+      />
+    </form>
+  );
+};
+
+export default BudgetForm;
